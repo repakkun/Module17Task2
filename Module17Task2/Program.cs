fe0c36312a5ff982bce1e4691832645340c90ccc
rePakkun <cj.ilya@yandex.ru>
2021-01-21 23:22:37 +0300

    Добавьте файлы проекта.

---
+++ Module17Task2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module17Task2
+{   
+    class NumberReader
+    {
+        public delegate void NumEnteredDelegate(int number);
+        public event NumEnteredDelegate NumEnteredEvent;
+        public void Read()
+        {
+            Console.WriteLine("1 или 2");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number != 1 && number != 2) throw new FormatException();
+
+            NumberEntered(number);
+        }
+
+        protected virtual void NumberEntered(int number)
+        {
+            NumEnteredEvent?.Invoke(number);
+        }
+    }
+    class Program
+    {      
+        static void Main(string[] args)
+        {
+            NumberReader numberReader = new NumberReader();
+            numberReader.NumEnteredEvent += ShowNumber;
+            {
+               while(true)
+                try
+                {
+                    numberReader.Read();               
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Неправильное значение");
+                }
+            }         
+        }
+        static void ShowNumber(int number)
+        {
+            string[] name = { "Антипов", "Георгиев","Биб", "Яковлев" };
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Введено 1");
+                    Array.Sort(name);
+                    for (int i = 0; i < name.Length; i++)
+                        Console.WriteLine("{0}", name[i]);
+                    break;
+                case 2:
+                    Console.WriteLine("Введено 2");
+                    Array.Sort(name);
+                    Array.Reverse(name);
+                    for (int i = 0; i < name.Length; i++)
+                        Console.WriteLine("{0}", name[i]);
+                    break;
+            }
+        }
+    }
+}
